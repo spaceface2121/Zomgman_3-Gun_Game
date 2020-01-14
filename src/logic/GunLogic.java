cc3bf4bf00e882619c3553408ae1f411036a76eb
Michael Mityushkin <misha.mityushk@gmail.com>
2020-01-13 21:27:35 -0500

    added saiga12 auto shotgun test it

--- src/logic/GunLogic.java
+++ src/logic/GunLogic.java
@@ -2,20 +2,20 @@ package logic;
 
 public class GunLogic {
     public static final byte SEMI = 0, BURST = 1, AUTO = 2, BUCKSHOT = 3;
-    public static final byte GLOCK = 0, UZI = 1, MP5 = 2, REVOLVER = 3, FAMAS = 4, AK = 5, SHOTGUN = 6, SNIPER = 7;
+    public static final byte GLOCK = 0, UZI = 1, MP5 = 2, REVOLVER = 3, FAMAS = 4, SAIGA = 5, AK = 6, SHOTGUN = 7, SNIPER = 8;
     public static final byte LIGHT_BULLET = 0, STANDARD_BULLET = 1, HEAVY_BULLET = 2, SHOTGUN_BULLET = 3;
 
-    //private static float[] X_VELS = {1, 1, 1, 1, 1, 1, 1, 1};
-    private static float[] X_VELS = {19, 23, 23, 28, 26, 24, 24, 40};
-    private static final byte[] FIRE_MODES = {SEMI, BURST, AUTO, SEMI, BURST, AUTO, BUCKSHOT, SEMI};
-    private static final byte[] BULLET_TYPES = {LIGHT_BULLET, LIGHT_BULLET, LIGHT_BULLET, HEAVY_BULLET, STANDARD_BULLET, STANDARD_BULLET, SHOTGUN_BULLET, HEAVY_BULLET};
-    private static final int[] SHOT_DELAY_MILLIS = {100, 20, 100, 600, 30, 120, 800, 1200};
-    private static final int[] RELOAD_TIME_MILLIS = {1300, 1400, 1700, 2000, 2300, 2400, 2500, 3500};
-    private static final byte[] DAMAGE_PER_HIT = {13, 13, 12, 35, 14, 17, 17, 50};
-    private static final byte[] MAG_CAPACITY = {16, 25, 30, 6, 30, 30, 5, 5};
-    private static int[] RANGE = {680, 720, 760, 1000, 800, 900, 500, 9999999}; //not "final" because these will have to be scaled based on screen resolution
-    private static float[] RELATIVE_BULLET_EXIT_POINT_Y = {-3, 0, 3, -8, 4, 1, -2, -5}; //CHANGE THESE AFTER MAKING GUN MODELS
-    private static float[] RELATIVE_X_POSITION = {12, 12, 0, 12, 5, 0, 3, 0};
+    //private static float[] X_VELS = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+    private static float[] X_VELS = {19, 23, 23, 28, 26, 25, 24, 24, 40};
+    private static final byte[] FIRE_MODES = {SEMI, BURST, AUTO, SEMI, BURST, AUTO, AUTO, BUCKSHOT, SEMI};
+    private static final byte[] BULLET_TYPES = {LIGHT_BULLET, LIGHT_BULLET, LIGHT_BULLET, HEAVY_BULLET, STANDARD_BULLET, SHOTGUN_BULLET, STANDARD_BULLET, SHOTGUN_BULLET, HEAVY_BULLET};
+    private static final int[] SHOT_DELAY_MILLIS = {100, 20, 100, 600, 30, 260, 120, 800, 1200};
+    private static final int[] RELOAD_TIME_MILLIS = {1300, 1400, 1700, 2000, 2300, 3000, 2400, 2500, 3500};
+    private static final byte[] DAMAGE_PER_HIT = {13, 13, 12, 35, 14, 20, 17, 17, 50};
+    private static final byte[] MAG_CAPACITY = {16, 25, 30, 6, 30, 10, 30, 5, 5};
+    private static int[] RANGE = {680, 720, 760, 1000, 800, 500, 900, 400, 9999999}; //not "final" because these will have to be scaled based on screen resolution
+    private static float[] RELATIVE_BULLET_EXIT_POINT_Y = {-3, 0, 3, -8, 4, -3, 1, -2, -5};
+    private static float[] RELATIVE_X_POSITION = {12, 12, 0, 12, 5, 2, 0, 3, 0};
 
     private static final int BURST_DELAY = 150;
 
@@ -34,6 +34,7 @@ public class GunLogic {
             case MP5: return (float)(Math.random() * 3 - 1.5);
             case UZI:
             case FAMAS:
+            case SAIGA:
             case AK: return (float)(Math.random() * 2 - 1);
             case REVOLVER:
             case SNIPER:
--- src/logic/PlayerLogic.java
+++ src/logic/PlayerLogic.java
@@ -8,10 +8,10 @@ import objects.Player;
 public class PlayerLogic {
     public static byte HAND1 = 0, HAND2 = 1, PLAYER1 = 0, PLAYER2 = 1;
     private static float MAX_X_VEL = 7, MIN_X_VEL = 3, Y_VEL = -12, Y_ACCELERATION = (float)0.5, X_ACCELERATION = (float)0.4;
-    private static float[] relativeRightHandX = {15, 20, 28, 20, 30, 25, 40, 43}; //absolute value of where the right hand would be relative to the gun (x)
-    private static float[] relativeLeftHandX = {-6, -6, -13, -8, -8, -7, -11, -11}; //same thing but left hand
-    private static float[] relativeRightHandY = {16, 18, 19, 17, 19, 16, 16, 16}; //absolute value of where the right hand would be relative to the gun (y)
-    private static float[] relativeLeftHandY = {22, 16, 23, 27, 20, 18, 18, 20}; //same thing but left hand
+    private static float[] relativeRightHandX = {15, 20, 28, 20, 30, 30, 25, 40, 43}; //absolute value of where the right hand would be relative to the gun (x)
+    private static float[] relativeLeftHandX = {-6, -6, -13, -8, -8, -9, -7, -11, -11}; //same thing but left hand
+    private static float[] relativeRightHandY = {16, 18, 19, 17, 19, 13, 16, 16, 16}; //absolute value of where the right hand would be relative to the gun (y)
+    private static float[] relativeLeftHandY = {22, 16, 23, 27, 20, 15, 18, 18, 20}; //same thing but left hand
     //actually fill in these values nig
 
     public static float getRelativeRightHandX(byte gunType) {
