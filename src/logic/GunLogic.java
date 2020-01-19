bb0e0adaf7bb642095b922c8bf39bfb8be520319
Sean <sean.vasserman@gmail.com>
2020-01-19 17:29:43 -0500

    gunlogic comments

--- src/logic/GunLogic.java
+++ src/logic/GunLogic.java
@@ -2,25 +2,46 @@ package logic;
 
 import main.Game;
 
+/**
+ * All static class that stores arrays of static variables for guns and contains accessor methods for these variables
+ */
 public class GunLogic {
+
+    /**
+     * The types of fire rate, guns, and bullets
+     */
     public static final byte SEMI = 0, BURST = 1, AUTO = 2, BUCKSHOT = 3;
     public static final byte GLOCK = 0, UZI = 1, MP5 = 2, REVOLVER = 3, FAMAS = 4, SAIGA = 5, AK = 6, SHOTGUN = 7, SNIPER = 8;
     public static final byte LIGHT_BULLET = 0, STANDARD_BULLET = 1, HEAVY_BULLET = 2, SHOTGUN_BULLET = 3;
 
-    //private static float[] X_VELS = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-    private static float[] X_VELS = {19, 20, 23, 28, 26, 25, 24, 24, 40};
+    /**
+     * Arrays for constant gun properties for each type of gun
+     */
     private static final byte[] FIRE_MODES = {SEMI, BURST, AUTO, SEMI, BURST, AUTO, AUTO, BUCKSHOT, SEMI};
     private static final byte[] BULLET_TYPES = {LIGHT_BULLET, LIGHT_BULLET, LIGHT_BULLET, HEAVY_BULLET, STANDARD_BULLET, SHOTGUN_BULLET, STANDARD_BULLET, SHOTGUN_BULLET, HEAVY_BULLET};
     private static final int[] SHOT_DELAY_MILLIS = {100, 20, 100, 600, 30, 200, 120, 800, 1200};
     private static final int[] RELOAD_TIME_MILLIS = {1300, 1400, 1700, 2000, 2300, 3000, 2400, 2500, 3500};
     private static final byte[] DAMAGE_PER_HIT = {13, 13, 12, 35, 14, 20, 17, 17, 50};
     private static final byte[] MAG_CAPACITY = {16, 25, 30, 6, 30, 10, 30, 5, 5};
-    private static int[] RANGE = {680, 720, 760, 1000, 800, 500, 900, 400, 9999999}; //not "final" because these will have to be scaled based on screen resolution
+
+    /**
+     * Arrays for variable gun properties for each type of gun (not final because they are scaled based on screen resolution)
+     */
+    //private static float[] X_VELS = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+    private static float[] X_VELS = {19, 20, 23, 28, 26, 25, 24, 24, 40};
+    private static int[] RANGE = {680, 720, 760, 1000, 800, 500, 900, 400, 9999999};
     private static float[] RELATIVE_BULLET_EXIT_POINT_Y = {-3, 0, 0, -8, 4, -3, 1, -2, -5};
     private static float[] RELATIVE_X_POSITION = {12, 12, 0, 12, 5, 2, 0, 3, 0};
 
+    /**
+     * Burst delay is constant for all burst guns
+     */
     private static final int BURST_DELAY = 150;
 
+
+    /**
+     * Accessor method for bullet X velocity based on the type of gun and player direction
+     */
     public static float getxVel(byte type, boolean dir) {
         if (dir) { //facing right
             return X_VELS[type];
@@ -29,6 +50,9 @@ public class GunLogic {
         }
     }
 
+    /**
+     * Accessor method for bullet Y velocity (bullet spread) based on the type of gun
+     */
     public static float getyVel(byte type) {
         //return 0;
         switch (type) {
@@ -41,10 +65,13 @@ public class GunLogic {
             case REVOLVER:
             case SNIPER:
             case SHOTGUN: return 0;
-            default: return 100; //it forces me to provide a return statement what the gay
+            default: return 100; //mandatory return statement, never called
         }
     }
 
+    /**
+     * Accessor methods for all array stored variables, based on type of gun
+     */
     public static byte getFireMode(byte type) {
         return FIRE_MODES[type];
     }
@@ -73,10 +100,6 @@ public class GunLogic {
         return RANGE[type];
     }
 
-    public static int getBurstDelay() {
-        return BURST_DELAY;
-    }
-
     public static float getRelativeBulletExitPointY(byte type) {
         return RELATIVE_BULLET_EXIT_POINT_Y[type];
     }
@@ -85,6 +108,14 @@ public class GunLogic {
         return RELATIVE_X_POSITION[type];
     }
 
+    /**
+     * Accessor method for burst delay
+     */
+    public static int getBurstDelay() { return BURST_DELAY; }
+
+    /**
+     * Scaling method for all fields based on screen resolution
+     */
     public static void generateScaledProperties(float scaleX, float scaleY) {
         for (int i = 0; i < RANGE.length; i++) { //all the arrays are the same length
             RANGE[i] = (int)(RANGE[i] * scaleX);
