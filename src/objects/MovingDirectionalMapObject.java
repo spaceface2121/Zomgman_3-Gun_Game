f17e66493edf5c06b07ea9b0cf20cb977aab2f87
Sean <sean.vasserman@gmail.com>
2020-01-19 21:58:39 -0500

    movingdirectionalmapobject javadoc

--- src/objects/MovingDirectionalMapObject.java
+++ src/objects/MovingDirectionalMapObject.java
@@ -5,15 +5,33 @@ import logic.CollisionLogic;
 import main.Main;
 import main.data.ObjectData;
 
+/**
+ * Most complex type of object that can be drawn on the screen
+ */
 public class MovingDirectionalMapObject extends DirectionalMapObject {
+    /**
+     * Velocity fields
+     */
     private float xVel, yVel;
 
+    /**
+     * Constructor for a MovingDirectionalMapObject
+     * @param objectData
+     * @param otherImage
+     * @param dir
+     * @param xVel
+     * @param yVel
+     */
     public MovingDirectionalMapObject(ObjectData objectData, Image otherImage, boolean dir, float xVel, float yVel) {
         super(objectData, otherImage, dir);
         setxVel(xVel);
         setyVel(yVel);
     }
 
+    /**
+     * Moves the object, depends on if it is allowed past the screen edge or not
+     * @param allowOutOfBounds
+     */
     public void move(boolean allowOutOfBounds) {
         ObjectData data = getObjectData();
 
@@ -45,28 +63,51 @@ public class MovingDirectionalMapObject extends DirectionalMapObject {
         }
     }
 
+    /**
+     * Checks if the object is past the screen edge
+     * @return
+     */
     public boolean isOutOfBounds() { //if the object is COMPLETELY off the map
         ObjectData data = getObjectData();
         return data.x <= -data.w || data.x >= Main.getGame().fullW || data.y < -data.h || data.y >= Main.getGame().fullH;
     }
 
+    /**
+     * Changes the object's direction
+     */
     public void changeDirection() {
         super.changeDirection();
         //xVel = -xVel;
     }
 
+    /**
+     * Accessor method for X velocity
+     * @return
+     */
     public float getxVel() {
         return xVel;
     }
 
+    /**
+     * Accessor method for Y velocity
+     * @return
+     */
     public float getyVel() {
         return yVel;
     }
 
+    /**
+     * Mutator method for X velocity
+     * @param xVel
+     */
     public void setxVel(float xVel) {
         this.xVel = xVel;
     }
 
+    /**
+     * Mutator method for Y velocity
+     * @param yVel
+     */
     public void setyVel(float yVel) {
         this.yVel = yVel;
     }
